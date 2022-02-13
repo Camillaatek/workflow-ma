ccb44f04bc9f90fb081898055efa94f397f7f6d7
Camillaatek <69843303+Camillaatek@users.noreply.github.com>
2022-02-13 23:37:33 +0100

    fix: remove code

--- workflow-ma-ca/src/utils/api.js
+++ workflow-ma-ca/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const BASE_URL = 'https://ma3-strapi.herokuapp.com/';
 export const MAKEUP_URL = 'makeup-brands';
-// export const POPULATE = '?populate=*';
+
 
 export const getData = (url) => {
   axios.get(url).then((response) => console.log(response.data.data));
